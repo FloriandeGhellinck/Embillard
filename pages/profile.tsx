1447f8896b5f3deaf743f8269ee29697b142ee2f
Florian de Ghellinck <f.deghellinck@gmail.com>
2022-07-13 17:54:23 +0200

    fix: screen height

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,7 +3,7 @@ import Welcomeprofile from "../components/welcome-profile";
 
 const Profile = () => {
   return (
-    <div className="flex h-screen text-black bg-gradient-to-r from-embie-orange-200 via-embie-yellow-200 to-embie-blue-light-300">
+    <div className="flex  text-black bg-gradient-to-r from-embie-orange-200 via-embie-yellow-200 to-embie-blue-light-300">
       <Welcomeprofile />
     </div>
   );
