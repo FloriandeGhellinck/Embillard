666598dd221c4dee55ca73ddd0565655dfed385f
Florian de Ghellinck <f.deghellinck@gmail.com>
2022-07-14 12:19:08 +0200

    fix: profile removal acces without cookie

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,19 @@
+import { getCookie } from "cookies-next";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Welcomeprofile from "../components/welcome-profile";
 
 const Profile = () => {
+  const router = useRouter();
+
+  const cookieValue = getCookie("isLoggedIn");
+
+  useEffect(() => {
+    if (!cookieValue) {
+      router.push("/");
+    }
+  }, [cookieValue, router]);
+
   return (
     <div className="flex text-black bg-gradient-to-r from-embie-orange-200 via-embie-yellow-200 to-embie-blue-light-300">
       <Welcomeprofile />
